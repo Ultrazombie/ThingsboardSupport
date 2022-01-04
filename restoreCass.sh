f114c4b7198cfeefe381e163fbf754912522cfd3
Artem Murenko <Murenko777@gmail.com>
2022-01-04 15:48:39 +0200

    Update restoreCass.sh

--- restoreCass.sh
+++ restoreCass.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-DIR=/home/support/tb-pe-3.2.2/cassandraBackup/thingsboard/ #type here your backup thingsboard dir
+#Make sure that script is located  inside backup directory near ts_kv_* files.
+mv ./ts_kv_cf* ./ts_kv_cf
+mv ./ts_kv_partitions_cf* ./ts_kv_partitions_cf
 
-mv ${DIR}ts_kv_cf* ${DIR}ts_kv_cf
-mv ${DIR}ts_kv_partitions_cf* ${DIR}ts_kv_partitions_cf
-
-sstableloader --verbose --nodes 127.0.0.1 ${DIR}ts_kv_partitions_cf/
-sstableloader --verbose --nodes 127.0.0.1 ${DIR}ts_kv_cf/
+sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_partitions_cf/
+sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_cf/
