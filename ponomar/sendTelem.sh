8d5c4c6b76b483c0944a182743f36c934aedc430
pon0mar <a.ponomarev.e@gmail.com>
2022-01-04 15:57:35 +0200

    update telemetry script

--- ponomar/sendTelem.sh
+++ ponomar/sendTelem.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-HOST_NAME=localhost
+HOST_NAME=127.0.0.1
 
-ACCESS_TOKEN=T2_TEST_TOKEN
+ACCESS_TOKEN1=T1_TEST_TOKEN
+ACCESS_TOKEN2=T2_TEST_TOKEN
+ACCESS_TOKEN3=C1_TEST_TOKEN
+ACCESS_TOKEN_DH=DHT11_DEMO_TOKEN
+ACCESS_TOKEN_RAS=RASPBERRY_PI_DEMO_TOKEN
+
+
 
 #for (( a = 1; a < 30; a++ ))
 while true
 do
-    mosquitto_pub -d -q 1 -h "$HOST_NAME" -p "1883" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN" -m {"temperature":"${RANDOM:0:2}"}
-    sleep 1s
+    mosquitto_pub -d -q 1 -h "$HOST_NAME" -p "1883" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN1" -m "{"temperature":"${RANDOM:0:2}"}"
+    mosquitto_pub -d -q 1 -h "$HOST_NAME" -p "1883" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN2" -m {"temperature":"${RANDOM:0:2}"}
+    mosquitto_pub -d -q 1 -h "$HOST_NAME" -p "1883" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN3" -m {"c1":"${RANDOM:0:2}"}
+    mosquitto_pub -d -q 1 -h "$HOST_NAME" -p "1883" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN_DH" -m {"DH":"${RANDOM:0:2}"}
+    mosquitto_pub -d -q 1 -h "$HOST_NAME" -p "1883" -t "v1/devices/me/telemetry" -u "$ACCESS_TOKEN_RAS" -m {"RAS":"${RANDOM:0:2}"}
+    sleep 0.001
 done 
 
