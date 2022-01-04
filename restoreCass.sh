acf9b221c458f03816e2cc608ceeb1c954161049
pon0mar <a.ponomarev.e@gmail.com>
2022-01-04 16:15:12 +0200

    Add untar

--- restoreCass.sh
+++ restoreCass.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+patch="/tmp/backup/"
+backup=${patch}"PASTE_BACKUP_FILENAME"
+
+
+cd patch || exit 
+sudo systemctl stop cassandra
+sudo mkdir untar && cd $_
+sudo tar -xvf $backup
 
 #Make sure that script is located  inside backup directory near ts_kv_* files.
-mv ./ts_kv_cf* ./ts_kv_cf
-mv ./ts_kv_partitions_cf* ./ts_kv_partitions_cf
+sudo mv ./ts_kv_cf* ./ts_kv_cf
+sudo mv ./ts_kv_partitions_cf* ./ts_kv_partitions_cf
 
 sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_partitions_cf/
 sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_cf/
+
+cd ..
+sudo rm -rf untar
