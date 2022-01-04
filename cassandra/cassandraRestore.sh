41e5f0f0ec38e8f51425fb8f11d467e34ae2ddb1
Sehii <meleshko-sergiy@ukr.net>
2022-01-04 16:36:52 +0200

    fix conflicts with naming

--- cassandra/cassandraRestore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo systemctl stop cassandra
-read -p "Please Enter Path: " -r patch
-cd /var/lib/cassandra/ || exit
-sudo tar -xvf $patch
-sudo chown -R cassandra. /var/lib/cassandra/
-
-sudo systemctl start cassandra
--- cassandra/restoreCass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=/home/support/tb-pe-3.2.2/cassandraBackup/thingsboard/ #type here your backup thingsboard dir
-
-mv ${DIR}ts_kv_cf* ${DIR}ts_kv_cf
-mv ${DIR}ts_kv_partitions_cf* ${DIR}ts_kv_partitions_cf
-
-sstableloader --verbose --nodes 127.0.0.1 ${DIR}ts_kv_partitions_cf/
-sstableloader --verbose --nodes 127.0.0.1 ${DIR}ts_kv_cf/
