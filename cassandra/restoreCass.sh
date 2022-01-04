42be7a66953769a8980967351bcbf93f44faceb9
pon0mar <a.ponomarev.e@gmail.com>
2022-01-04 17:30:22 +0200

    update restore

--- cassandra/restoreCass.sh
+++ cassandra/restoreCass.sh
@@ -14,7 +14,7 @@ CF=$(sudo find . -name "ts_kv_cf")
 PARTITIONS_CF=$(sudo find . -name "ts_kv_partitions_cf")
 
 sudo mv ${CF}* ./ts_kv_cf
-sudo mv ${PARTITIONS_CF} ./ts_kv_partitions_cf
+sudo mv ${PARTITIONS_CF}* ./ts_kv_partitions_cf
 
 sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_partitions_cf/
 sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_cf/
