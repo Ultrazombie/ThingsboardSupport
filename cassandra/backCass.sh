f9fed89713cd484d265be8a4a3006819bc433a20
pon0mar <a.ponomarev.e@gmail.com>
2022-01-05 11:49:18 +0200

    update cassandra scripts

--- cassandra/backCass.sh
+++ cassandra/backCass.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 current_date=$(date +'%m-%d-%y_%H:%M')
 
 avail_in_Kb=$(df -BK / | tail -1 | awk '{print $4}' | sed -r  's/^[^0-9]*([0-9]+).*/\1/')
 filesize_in_bytes=$(du -s /var/lib/cassandra/data/thingsboard |  awk '{print int($1)}')
-avail=`echo "scale=6; $avail_in_kb/1024/1024" | bc -l`
-filesize=`echo "scale=6; $filesize_in_bytes/1024/1024/1024" | bc -l`
+avail=$(echo "scale=6; $avail_in_Kb/1024/1024" | bc -l)
+filesize=$(echo "scale=6; $filesize_in_bytes/1024/1024/1024" | bc -l)
 echo "$avail"
 echo "$filesize"
 
-if [ `echo "$avail<=$filesize" | bc` -ge 1 ]
+if [ $(echo "$avail<=$filesize" | bc) -ge 1 ]
 then
 echo "not enought space"
 else
@@ -17,8 +18,9 @@ echo "enought space"
 nodetool flush
 
 mkdir -p /tmp/backup
-sudo tar -cvf /tmp/backup/$current_date.cassandra.tar /var/lib/cassandra/data/thingsboard
 cqlsh 127.0.0.1 -e "DESCRIBE KEYSPACE thingsboard;" > /tmp/backup/thingsboard-describe.txt
 
+sudo tar -cvf /tmp/backup/$current_date.cassandra.tar /var/lib/cassandra/data/thingsboard /tmp/backup/thingsboard-describe.txt
+rm -rf /tmp/backup/thingsboard-describe.txt
 
 fi
--- cassandra/restoreCass.sh
+++ cassandra/restoreCass.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-PATCH="/pon0mar/backup/"
-BACKUP=${PATCH}"01-04-22_16_26.cassandra.tar"
+PATCH="/tmp/backup/"
+BACKUP=${PATCH}"01-05-22_11:44.cassandra.tar"
+
 
 cd $PATCH || exit 
-cqlsh 127.0.0.1 < thingsboard-describe.txt
 
-
-sudo mkdir thingsboard && cd $_
+sudo mkdir thingsboard && cd thingsboard
 sudo tar -xvf $BACKUP
+
+
 
 
 CF=$(sudo find . -name "ts_kv_cf*")
 PARTITIONS_CF=$(sudo find . -name "ts_kv_partitions_cf*")
+DESCRIBE=$(sudo find . -name "thingsboard-describe.txt")
 
+cqlsh 127.0.0.1 < $DESCRIBE
 sudo mv ${CF} ./ts_kv_cf
 sudo mv ${PARTITIONS_CF} ./ts_kv_partitions_cf
 
@@ -21,4 +24,4 @@ cd $PATCH || exit
 sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_partitions_cf
 sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_cf
 
-sudo rm -rf thingsboard 
+ sudo rm -rf thingsboard   
