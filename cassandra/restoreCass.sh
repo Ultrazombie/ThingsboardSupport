b42521a24e0248bddf6f4cfe36590fa59251646e
pon0mar <a.ponomarev.e@gmail.com>
2022-01-05 16:53:47 +0200

    Add ts_kv_latest search

--- cassandra/restoreCass.sh
+++ cassandra/restoreCass.sh
@@ -9,6 +9,7 @@ sudo tar -xvf $BACKUP
 
 CF=$(sudo find . -name "ts_kv_cf*")
 PARTITIONS_CF=$(sudo find . -name "ts_kv_partitions_cf*")
+LATEST=$(sudo find . -name "ts_kv_latest*")
 DESCRIBE=$(sudo find . -name "thingsboard-describe.txt")
 
 cqlsh 127.0.0.1 < $DESCRIBE
@@ -16,6 +17,12 @@ sudo mv ${CF} ./ts_kv_cf
 sudo mv ${PARTITIONS_CF} ./ts_kv_partitions_cf
 
 cd $PATCH || exit
+
+if [[ $LATEST ]]
+then
+  sudo mv ${LATEST} ./thingsboard/ts_kv_latest
+  sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_latest
+fi
 
 sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_partitions_cf
 sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_cf
