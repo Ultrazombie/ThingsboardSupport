b335c19a03f57c97cc647d80541cadc6e00deb6d
pon0mar <a.ponomarev.e@gmail.com>
2022-01-05 10:27:02 +0200

    update restore script

--- cassandra/restoreCass.sh
+++ cassandra/restoreCass.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-PATCH="/tmp/backup/"
-BACKUP=${PATCH}"PASTE_BACKUP_FILENAME"
+PATCH="/pon0mar/backup/"
+BACKUP=${PATCH}"01-04-22_16_26.cassandra.tar"
 
 cd $PATCH || exit 
 cqlsh 127.0.0.1 < thingsboard-describe.txt
 
 
-sudo systemctl stop cassandra
 sudo mkdir thingsboard && cd $_
 sudo tar -xvf $BACKUP
 
-CF=$(sudo find . -name "ts_kv_cf")
-PARTITIONS_CF=$(sudo find . -name "ts_kv_partitions_cf")
 
-sudo mv ${CF}* ./ts_kv_cf
-sudo mv ${PARTITIONS_CF}* ./ts_kv_partitions_cf
+CF=$(sudo find . -name "ts_kv_cf*")
+PARTITIONS_CF=$(sudo find . -name "ts_kv_partitions_cf*")
 
-sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_partitions_cf/
-sstableloader --verbose --nodes 127.0.0.1 ./ts_kv_cf/
+sudo mv ${CF} ./ts_kv_cf
+sudo mv ${PARTITIONS_CF} ./ts_kv_partitions_cf
+
+cd $PATCH || exit
+
+sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_partitions_cf
+sudo sstableloader --verbose --nodes 127.0.0.1 ./thingsboard/ts_kv_cf
 
 cd ..
-sudo rm -rf thingsboard
+sudo rm -rf thingsboard 
