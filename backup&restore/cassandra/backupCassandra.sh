cf02192e1356f3635b7aadf377ef4c3a162c55e1
pon0marev <a.ponomarev.e@gmail.com>
2022-02-14 16:05:57 +0200

    change date variable, directory, if

--- backup&restore/cassandra/backupCassandra.sh
+++ backup&restore/cassandra/backupCassandra.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -eu
-LOG="/tmp/backup/backup_cassandra/BackupCassandra.log"
-BACKUP_PATH="/tmp/backup/backup_cassandra/"
+BACKUP_PATH="/home/${USER}/backup/cassandra/"
+LOG="${BACKUP_PATH}BackupCassandra.log"
 DB="/var/lib/cassandra/data/thingsboard"
-
-WEBHOOK_FILE="/tmp/backup/backup_cassandra/WebhookMessageCassandra.log"
+WEBHOOK_FILE="${BACKUP_PATH}WebhookMessageCassandra.log"
 WEBHOOK="https://yourwebhookendpoint.com/"
 
 mkdir -p $BACKUP_PATH
@@ -14,8 +12,7 @@ exec  2> >(sudo tee -ia $LOG $WEBHOOK_FILE >& 2)
 truncate -s 0 $WEBHOOK_FILE
 find $BACKUP_PATH -mtime +3 -exec rm -f {} \; # delete backup older than * days
 
-CUR_DATE=$(date +'%m-%d-%y_%H:%M')
-echo -e "\n-------- Start Cassandra backup process at ${CUR_DATE} --------"
+echo -e "\n---- Start Cassandra backup process at $(date +'%m-%d-%y_%H:%M') ----"
 
 AVAIL=$(df -m / | awk '{print $4}' | tail -1 )
 FILESIZE=$(sudo du -sm $DB |  awk '{print int($1)}')
@@ -23,7 +20,7 @@ FILESIZE=$(sudo du -sm $DB |  awk '{print int($1)}')
 echo "Free space: ${AVAIL} Mb"
 echo "Cassandra DB size: ${FILESIZE} Mb"
 
-if [ $(echo "$AVAIL<=$FILESIZE" | bc) -ge 1 ]
+if [ $FILESIZE -ge $AVAIL ]
 then
   echo " Not enought free space"
 else
@@ -32,19 +29,19 @@ else
     nodetool flush
     cqlsh 127.0.0.1 -e "DESCRIBE KEYSPACE thingsboard;" > ${BACKUP_PATH}thingsboard-describe.txt
 
-    TARFILE=$BACKUP_PATH$CUR_DATE.cassandra.tar
+    TARFILE=${BACKUP_PATH}$(date +'%m-%d-%y_%H:%M').cassandra.tar
     sudo tar -cf "$TARFILE" -P /var/lib/cassandra/data/thingsboard ${BACKUP_PATH}thingsboard-describe.txt
     rm -rf ${BACKUP_PATH}thingsboard-describe.txt
 
     TARFILE_SIZE=$(du -m "$TARFILE" | awk '{print $1}')
     echo "Completed. Backup file size: ${TARFILE_SIZE} Mb"
     MINSIZE=1
-    if [ $(echo "$TARFILE_SIZE<=$MINSIZE" | bc) -ge 1 ]
+    if [ 1 -ge $TARFILE_SIZE ]
     then
-      echo "WARNING. Backup file is less then 1 Mb"
+      echo "WARN. Backup file is less then 1 Mb"
     fi
 fi
-echo -e "------- Backup process finished at $(date +'%m-%d-%y_%H:%M') -------"
+echo -e "------- Backup process finished at $(date +'%m-%d-%y_%H:%M') -------\n"
 
 WEBHOOK_DATA="{\"text\":\"$(cat $WEBHOOK_FILE)\"}"
 curl -X POST -H 'Content-type: application/json' --data "$WEBHOOK_DATA" "$WEBHOOK"
