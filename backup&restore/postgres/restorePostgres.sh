bc63ae1aaa9d8cf0cc44b3b960cc5aedca0b63b0
pon0marev <a.ponomarev.e@gmail.com>
2022-03-24 17:00:54 +0200

    add host

--- backup&restore/postgres/restorePostgres.sh
+++ backup&restore/postgres/restorePostgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-psql -U postgres -c "drop database thingsboard"
-psql -U postgres -c "create database thingsboard"
+psql -U postgres -h localhost -c "drop database thingsboard"
+psql -U postgres -h localhost -c "create database thingsboard"
 psql -U postgres -d thingsboard -W< /home/${USER}/backup/postgres/.thingsboard.sql.bak
