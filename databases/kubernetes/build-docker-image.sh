d6b397dc65bc53b5fa82c81e837b820fdecefb11
pon0marev <a.ponomarev.e@gmail.com>
2022-04-22 19:18:07 +0300

    add cassandra backup in kubernetes

--- databases/kubernetes/build-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Go to dockerfile directory
-cd tb/backup/dockerfiledir || exit
-
-# Replace YOU_DOCKERHUB_NAME and VERSION
-docker build -t YOU_DOCKERHUB_NAME/k8s-backup:VERSION .
-docker push YOU_DOCKERHUB_NAME/k8s-backup:VERSION
