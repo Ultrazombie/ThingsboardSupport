200e4b3c3d5759d03a395753fefb901252e27592
pon0marev <a.ponomarev.e@gmail.com>
2022-05-11 13:51:50 +0300

    Change dockerfile dir

--- databases/cassandra-in-kubernetes/build-docker-image.sh
+++ databases/cassandra-in-kubernetes/build-docker-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go to dockerfile directory
-cd tb/backup/dockerfiledir || exit
+cd ./dockerfiledir || exit
 
 # Replace YOU_DOCKERHUB_NAME and VERSION
 docker build -t YOU_DOCKERHUB_NAME/k8s-backup:VERSION .
